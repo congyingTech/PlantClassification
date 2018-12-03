db6c682173b290c7682d1d6518af23ee1c971b1f
wangcongying <wangcongying@kuaishou.com>
2018-12-03 17:37:46 +0800

    core code

--- CoreCode/algorithm/__init__.py
+++ CoreCode/algorithm/__init__.py
@@ -9,3 +9,22 @@ import functools
 import tensorflow as tf
 from tensorflow.python.ops import rnn, array_ops
 from tensorflow.contrib.rnn import GRUCell, DropoutWrapper, MultiRNNCell
+import numpy as np
+
+# 定义一个lazy的装饰器
+
+
+def lazy_property(func):
+    attribute = '_' + func.__name__
+
+    @property
+    @functools.wraps(func)
+    def wrapper(self):
+        if not hasattr(self, attribute):
+            setattr(self, attribute, func(self))
+        return getattr(self, attribute)
+    return wrapper
+
+
+class VariableSequenceClassification(object):
+    pass
